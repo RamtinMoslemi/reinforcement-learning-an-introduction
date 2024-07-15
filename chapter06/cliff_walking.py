65542b7670539a89007691f210dedf64c8b92eda
RamtinMoslemi <ramtin4moslemi@gmail.com>
2024-07-15 14:03:54 +0330

    Add visualization

--- chapter06/cliff_walking.py
+++ chapter06/cliff_walking.py
@@ -11,6 +11,7 @@ import seaborn as sns
 import gymnasium as gym
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+from utils import visualize
 
 # probability for exploration
 EPSILON = 0.1
@@ -21,14 +22,11 @@ ALPHA = 0.5
 # discount factor for Q-Learning and Expected Sarsa
 GAMMA = 1
 
-# there are 48 different states in the cliff walking environment
-OBSERVATION_SPACE = 48
-
-# there are 4 different actions in the cliff walking environment (up, right, down, left)
-ACTION_SPACE = 4
+# environment
+CLIFF_WALKING = gym.make('CliffWalking-v0')
 
 # where to save the images
-image_dir = '../images/chapter06/'
+IMAGE_DIR = '../images/chapter06/'
 
 
 def plot_heatmap(q_values, title, saving_format='.png'):
@@ -46,16 +44,17 @@ def plot_heatmap(q_values, title, saving_format='.png'):
     plt.axis('equal')
     plt.title(title, fontsize=24)
     im = sns.heatmap(value_max, cmap="inferno", annot=labels, annot_kws={'fontsize': 26}, fmt='s')
-    im.figure.savefig(image_dir + title + saving_format, bbox_inches='tight', dpi=200)
+    im.figure.savefig(IMAGE_DIR + title + saving_format, bbox_inches='tight', dpi=200)
 
 
-def choose_action(state: int, q_values: np.ndarray) -> int:
+def choose_action(state: int, q_values: np.ndarray, env: gym.Env) -> int:
     """Epsilon-greedy policy: selects the maximum value action with probability (1-epsilon) and selects randomly with
     epsilon probability.
 
     Args:
         state (integer): current state
         q_values (ndarray): current value function of shape (n_states, n_actions)
+        env (Env): gymnasium environment
 
     Returns:
         action (integer): the chosen action
@@ -63,16 +62,16 @@ def choose_action(state: int, q_values: np.ndarray) -> int:
     if np.random.random() > EPSILON:  # greedy action
         action = np.argmax(q_values[state])
     else:  # random action
-        action = np.random.choice(q_values.shape[1])
+        action = env.action_space.sample()
     return action
 
 
-def q_learning(n_episodes: int, plot_q_val: bool = True) -> (np.ndarray, np.ndarray):
-    """Q-Learning: runs the Q-Learning algorithm on the Cliff Walking environment.
+def q_learning(n_episodes: int, env: gym.Env) -> (np.ndarray, np.ndarray):
+    """Q-Learning: runs the Q-learning algorithm with epsilon-greedy policy on gym environment.
 
     Args:
         n_episodes (integer): the number of episodes we train the model
-        plot_q_val (bool): plot the Q-values
+        env (Env): gym environment
 
     Returns:
         episode_rewards (ndarray): the sum of rewards in each episode
@@ -80,33 +79,28 @@ def q_learning(n_episodes: int, plot_q_val: bool = True) -> (np.ndarray, np.ndar
     """
     episode_rewards = np.empty(n_episodes)
 
-    env = gym.make('CliffWalking-v0')
-    q = np.zeros(shape=(OBSERVATION_SPACE, ACTION_SPACE))
+    q = np.zeros(shape=(env.observation_space.n, env.action_space.n))
 
     for episode_i in range(n_episodes):
         state, info = env.reset()
         episode_reward_sum, terminal = 0, False
         while not terminal:
-            action = choose_action(state, q)
+            action = choose_action(state, q, env)
             next_state, reward, terminated, truncated, info = env.step(action)
             q[state, action] += ALPHA * (reward + GAMMA * np.max(q[next_state]) - q[state, action])
             state = next_state
             episode_reward_sum += reward
             terminal = terminated or truncated
         episode_rewards[episode_i] = episode_reward_sum
-
-    if plot_q_val:
-        plot_heatmap(q, f'Optimal Q-Learning path for Cliff Walking after {n_episodes} episodes')
-
     return episode_rewards, q
 
 
-def sarsa(n_episodes: int, plot_q_val: bool = True) -> (np.array, np.ndarray):
-    """Sarsa: runs the Sarsa algorithm on the Cliff Walking environment.
+def sarsa(n_episodes: int, env: gym.Env) -> (np.array, np.ndarray):
+    """Sarsa: runs the Sarsa algorithm with epsilon-greedy policy on gym environment.
 
     Args:
         n_episodes (integer): the number of episodes we train the model
-        plot_q_val (bool): plot the Q-values
+        env (Env): gymnasium environment
 
     Returns:
         episode_rewards (ndarray): the sum of rewards in each episode
@@ -114,39 +108,35 @@ def sarsa(n_episodes: int, plot_q_val: bool = True) -> (np.array, np.ndarray):
     """
     episode_rewards = np.empty(n_episodes)
 
-    env = gym.make('CliffWalking-v0')
-    q = np.zeros(shape=(OBSERVATION_SPACE, ACTION_SPACE))
+    q = np.zeros(shape=(env.observation_space.n, env.action_space.n))
 
     for episode_i in range(n_episodes):
         state, info = env.reset()
         episode_reward_sum, terminal = 0, False
-        action = choose_action(state, q)
+        action = choose_action(state, q, env)
         while not terminal:
             next_state, reward, terminated, truncated, info = env.step(action)
-            next_action = choose_action(next_state, q)
+            next_action = choose_action(next_state, q, env)
             q[state, action] += ALPHA * (reward + GAMMA * q[next_state, next_action] - q[state, action])
             state, action = next_state, next_action
             episode_reward_sum += reward
             terminal = terminated or truncated
         episode_rewards[episode_i] = episode_reward_sum
-
-    if plot_q_val:
-        plot_heatmap(q, f'Optimal Sarsa path for Cliff Walking after {n_episodes} episodes')
-
     return episode_rewards, q
 
 
 # Sarsa converges to the safe path, while Q-Learning converges to the optimal path
 def figure_6_4(episodes=500, runs=1000, saving_format='.png'):
-    sarsa_rewards, sarsa_q = np.zeros(episodes), np.zeros(shape=(OBSERVATION_SPACE, ACTION_SPACE))
-    q_learning_rewards, q_learning_q = np.zeros(episodes), np.zeros(shape=(OBSERVATION_SPACE, ACTION_SPACE))
+    q_shape = (CLIFF_WALKING.observation_space.n, CLIFF_WALKING.action_space.n)
+    sarsa_rewards, sarsa_q = np.zeros(episodes), np.zeros(shape=q_shape)
+    q_learning_rewards, q_learning_q = np.zeros(episodes), np.zeros(shape=q_shape)
 
     # repeat for multiple runs to achieve smoother results
     for _ in tqdm(range(runs)):
-        sarsa_rewards_, sarsa_q_ = sarsa(episodes, False)
+        sarsa_rewards_, sarsa_q_ = sarsa(episodes, CLIFF_WALKING)
         sarsa_rewards += sarsa_rewards_
         sarsa_q += sarsa_q_
-        q_learning_rewards_, q_learning_q_ = q_learning(episodes, False)
+        q_learning_rewards_, q_learning_q_ = q_learning(episodes, CLIFF_WALKING)
         q_learning_rewards += q_learning_rewards_
         q_learning_q += q_learning_q_
 
@@ -158,71 +148,72 @@ def figure_6_4(episodes=500, runs=1000, saving_format='.png'):
 
     # draw reward curves
     plt.plot(sarsa_rewards, label='Sarsa', color='deepskyblue')
-    plt.plot(q_learning_rewards, label='Q-Learning', color='red')
+    plt.plot(q_learning_rewards, label='Q-learning', color='red')
     plt.xlabel('Episodes')
     plt.ylabel('Sum of rewards during episode')
     plt.ylim([-100, 0])
     plt.legend()
 
-    plt.savefig(image_dir + 'figure_6_4' + saving_format, bbox_inches='tight', dpi=200)
+    plt.savefig(IMAGE_DIR + 'figure_6_4' + saving_format, bbox_inches='tight', dpi=200)
     plt.close()
 
     # display optimal policy
-    plot_heatmap(q_learning_q, f'Optimal Q-Learning path for Cliff Walking')
+    render_env = gym.make('CliffWalking-v0', render_mode='rgb_array')
     plot_heatmap(sarsa_q, f'Optimal Sarsa path for Cliff Walking')
+    visualize.make_gif(render_env, sarsa_q, 'sarsa_cliff_walking', image_dir=IMAGE_DIR)
+    plot_heatmap(q_learning_q, f'Optimal Q-learning path for Cliff Walking')
+    visualize.make_gif(render_env, q_learning_q, 'q-learning_cliff_walking', image_dir=IMAGE_DIR)
 
 
-#
-# # Due to limited capacity of calculation of my machine, I can't complete this experiment
-# # with 100,000 episodes and 50,000 runs to get the fully averaged performance
-# # However even I only play for 1,000 episodes and 10 runs, the curves looks still good.
-# def figure_6_6():
-#     step_sizes = np.arange(0.1, 1.1, 0.1)
-#     episodes = 1000
-#     runs = 10
-#
-#     ASY_SARSA = 0
-#     ASY_EXPECTED_SARSA = 1
-#     ASY_QLEARNING = 2
-#     INT_SARSA = 3
-#     INT_EXPECTED_SARSA = 4
-#     INT_QLEARNING = 5
-#     methods = range(0, 6)
-#
-#     performace = np.zeros((6, len(step_sizes)))
-#     for run in range(runs):
-#         for ind, step_size in tqdm(list(zip(range(0, len(step_sizes)), step_sizes))):
-#             q_sarsa = np.zeros((WORLD_HEIGHT, WORLD_WIDTH, 4))
-#             q_expected_sarsa = np.copy(q_sarsa)
-#             q_q_learning = np.copy(q_sarsa)
-#             for ep in range(episodes):
-#                 sarsa_reward = sarsa(q_sarsa, expected=False, step_size=step_size)
-#                 expected_sarsa_reward = sarsa(q_expected_sarsa, expected=True, step_size=step_size)
-#                 q_learning_reward = q_learning(q_q_learning, step_size=step_size)
-#                 performace[ASY_SARSA, ind] += sarsa_reward
-#                 performace[ASY_EXPECTED_SARSA, ind] += expected_sarsa_reward
-#                 performace[ASY_QLEARNING, ind] += q_learning_reward
-#
-#                 if ep < 100:
-#                     performace[INT_SARSA, ind] += sarsa_reward
-#                     performace[INT_EXPECTED_SARSA, ind] += expected_sarsa_reward
-#                     performace[INT_QLEARNING, ind] += q_learning_reward
-#
-#     performace[:3, :] /= episodes * runs
-#     performace[3:, :] /= 100 * runs
-#     labels = ['Asymptotic Sarsa', 'Asymptotic Expected Sarsa', 'Asymptotic Q-Learning',
-#               'Interim Sarsa', 'Interim Expected Sarsa', 'Interim Q-Learning']
-#
-#     for method, label in zip(methods, labels):
-#         plt.plot(step_sizes, performace[method, :], label=label)
-#     plt.xlabel('alpha')
-#     plt.ylabel('reward per episode')
-#     plt.legend()
-#
-#     plt.savefig('../images/figure_6_6.png')
-#     plt.close()
+# Due to limited capacity of calculation of my machine, I can't complete this experiment
+# with 100,000 episodes and 50,000 runs to get the fully averaged performance
+# However even I only play for 1,000 episodes and 10 runs, the curves looks still good.
+def figure_6_6():
+    step_sizes = np.arange(0.1, 1.1, 0.1)
+    episodes = 1000
+    runs = 10
+
+    ASY_SARSA = 0
+    ASY_EXPECTED_SARSA = 1
+    ASY_QLEARNING = 2
+    INT_SARSA = 3
+    INT_EXPECTED_SARSA = 4
+    INT_QLEARNING = 5
+    methods = range(0, 6)
+
+    performance = np.zeros((6, len(step_sizes)))
+    for run in range(runs):
+        for ind, step_size in tqdm(list(zip(range(0, len(step_sizes)), step_sizes))):
+            q_sarsa = np.zeros((WORLD_HEIGHT, WORLD_WIDTH, 4))
+            q_expected_sarsa = np.copy(q_sarsa)
+            q_q_learning = np.copy(q_sarsa)
+            for ep in range(episodes):
+                sarsa_reward = sarsa(q_sarsa, expected=False, step_size=step_size)
+                expected_sarsa_reward = sarsa(q_expected_sarsa, expected=True, step_size=step_size)
+                q_learning_reward = q_learning(q_q_learning, step_size=step_size)
+                performance[ASY_SARSA, ind] += sarsa_reward
+                performance[ASY_EXPECTED_SARSA, ind] += expected_sarsa_reward
+                performance[ASY_QLEARNING, ind] += q_learning_reward
+
+                if ep < 100:
+                    performance[INT_SARSA, ind] += sarsa_reward
+                    performance[INT_EXPECTED_SARSA, ind] += expected_sarsa_reward
+                    performance[INT_QLEARNING, ind] += q_learning_reward
+
+    performance[:3, :] /= episodes * runs
+    performance[3:, :] /= 100 * runs
+    labels = ['Asymptotic Sarsa', 'Asymptotic Expected Sarsa', 'Asymptotic Q-Learning',
+              'Interim Sarsa', 'Interim Expected Sarsa', 'Interim Q-Learning']
+
+    for method, label in zip(methods, labels):
+        plt.plot(step_sizes, performance[method, :], label=label)
+    plt.xlabel('alpha')
+    plt.ylabel('reward per episode')
+    plt.legend()
+
+    plt.savefig('../images/figure_6_6.png')
+    plt.close()
 
 
 if __name__ == '__main__':
-    environment = gym.make('CliffWalking-v0')
     figure_6_4()
---
+++ utils/visualize.py
@@ -0,0 +1,72 @@
+import gymnasium as gym
+import numpy as np
+import cv2
+from PIL import Image
+
+
+DEFAULT_DIR = '../images/'
+
+
+def greedy(q_value, state):
+    return np.argmax(q_value[state])
+
+
+def show_policy(env, q_value, act=greedy):
+    state, info = env.reset()
+    while True:
+        action = act(q_value, state)
+        state, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated:
+            break
+    env.close()
+
+
+def make_mp4(env, q_value, name='file_name', act=greedy, image_dir=DEFAULT_DIR):
+    assert env.render_mode == 'rgb_array'
+    frames = list()
+    state, info = env.reset()
+    while True:
+        frames.append(env.render())
+        action = act(q_value, state)
+        state, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated:
+            frames.append(env.render())
+            break
+    env.close()
+    height, width, layers = frames[0].shape
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 'mp4v' for .mp4, 'XVID' for .avi
+    video = cv2.VideoWriter(image_dir + name + '.mp4', fourcc, env.metadata['render_fps'], (width, height))
+
+    for frame in frames:
+        # Convert RGB to BGR for OpenCV
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        video.write(frame_bgr)
+
+    video.release()
+
+
+def make_gif(env, q_value, name='file_name', act=greedy, image_dir=DEFAULT_DIR):
+    assert env.render_mode == 'rgb_array'
+    frames = []
+    state, info = env.reset()
+    while True:
+        frame = env.render()
+        frames.append(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))  # Convert to BGR for OpenCV
+        action = act(q_value, state)
+        state, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated:
+            frame = env.render()
+            frames.append(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))  # Convert to BGR for OpenCV
+            break
+    env.close()
+
+    # Convert frames to PIL images
+    pil_frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) for frame in frames]
+
+    # Save as GIF
+    gif_path = f"{image_dir}{name}.gif"
+    duration = 1000 // env.metadata['render_fps']
+    pil_frames[0].save(
+        gif_path, save_all=True, append_images=pil_frames[1:], loop=0, duration=duration
+    )
